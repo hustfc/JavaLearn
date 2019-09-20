514068735d52371134cc698797ae45910dd45d55
hustfc <spfanc@gmail.com>
2019-09-20 17:41:50 +0800

    learnJava

---
+++ src/EnumTest.java
@@ -0,0 +1,17 @@
+/**
+ * @Author : fanc
+ * @Date : 2019-09-20 17:35
+ */
+public enum EnumTest {
+    /**
+     * NOT
+     */
+    NOT("haha"),
+    MILD,
+    MEDIUM,
+    HOT,
+    FLAMING;
+
+    private String value;
+
+}
