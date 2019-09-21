1ae98b4e135331dadca5a74e863fb03e699e12c5
hustfc <spfanc@gmail.com>
2019-09-21 10:28:51 +0800

    print

---
+++ src/com/fanc/Print.java
@@ -0,0 +1,14 @@
+package com.fanc;
+
+/**
+ * @Author : fanc
+ * @Date : 2019-09-21 09:45
+ */
+public class Print {
+    public static void print(Object obj) {
+        System.out.println(obj);
+    }
+    public static void printnb(Object obj) {
+        System.out.print(obj);
+    }
+}
---
+++ src/test/PrintTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static com.fanc.Print.*;
+/**
+ * @Author : fanc
+ * @Date : 2019-09-21 10:27
+ */
+public class PrintTest {
+    public static void main(String[] args) {
+        print("Available from now on!");
+        printnb(100);
+        print(32L);
+    }
+}
